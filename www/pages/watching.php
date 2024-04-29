8ae619fbab16590e907a739beec725fac735d901
yudum-y <yudumyilmaz32@gmail.com>
2024-04-29 11:36:23 +0200

    search, navigation und mehr

--- www/pages/watching.php
+++ www/pages/watching.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Watchlist</title>
     <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
     <?php include "../functions/navigation.php"; ?>
     <?php include "../functions/logo.php"; ?>
-
-    <div class="container-button" id="bigButton">
-        Add new series
+    <div class="container">
+        <h2>Watching</h2>
+        <!-- watchlistfunktion -->
     </div>
 </body>
 </html>
