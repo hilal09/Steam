da0b83ea67f50e71c70c106a8e363ce2016e550b
hilal09 <hilal.cubukcu02@gmail.com>
2024-05-06 21:34:10 +0200

    logout provisorisch

--- www/functions/navigation.php
+++ www/functions/navigation.php
@@ -1,5 +1,7 @@
 <nav class="navbar">
     <ul>
+    <a href="../functions/logout.php">Logout</a>
+
         <li><a href="../pages/dashboard.php" <?php if(basename($_SERVER['PHP_SELF']) == 'dashboard.php') echo 'class="active"'; ?>>Home</a></li>
         <li><a href="../pages/search.php" <?php if(basename($_SERVER['PHP_SELF']) == 'search.php') echo 'class="active"'; ?>>Search</a></li>
         <li><a href="../pages/profile.php" <?php if(basename($_SERVER['PHP_SELF']) == 'profile.php') echo 'class="active"'; ?>>Profile</a></li>
