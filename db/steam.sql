3a7e6b46da999dbdefdd1a425977dc1fe35981f4
hilal09 <hilal.cubukcu02@gmail.com>
2024-05-09 21:16:56 +0200

    NEW DATABASE and pictures for default_series

--- db/steam.sql
+++ db/steam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 08. Mai 2024 um 23:13
+-- Erstellungszeit: 09. Mai 2024 um 21:15
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -42,21 +42,21 @@ CREATE TABLE `default_series` (
 --
 
 INSERT INTO `default_series` (`id`, `title`, `year`, `seasons`, `genre`, `platform`, `picture_url`) VALUES
-(1, 'Breaking Bad', 2008, 5, 'Drama', 'Netflix', NULL),
-(2, 'Friends', 1994, 10, 'Comedy', 'HBO Max', NULL),
-(3, 'Stranger Things', 2016, 4, 'Sci-fi', 'Netflix', NULL),
-(4, 'BoJack Horseman', 2014, 6, 'Animation', 'Netflix', NULL),
-(5, 'Game of Thrones', 2011, 8, 'Action', 'HBO', NULL),
-(6, 'The Mandalorian', 2019, 2, 'Sci-fi', 'Disney+', NULL),
-(7, 'The Haunting of Hill House', 2018, 2, 'Horror', 'Netflix', NULL),
-(8, 'The Handmaid\'s Tale', 2017, 4, 'Drama', 'Hulu', NULL),
-(9, 'The Witcher', 2019, 2, 'Fantasy', 'Netflix', NULL),
-(10, 'Westworld', 2016, 3, 'Sci-fi', 'HBO', NULL),
-(11, 'Brooklyn Nine-Nine', 2013, 8, 'Comedy', 'NBC', NULL),
-(12, 'Loki', 2021, 1, 'Action', 'Disney+', NULL),
-(13, 'Rick and Morty', 2013, 5, 'Animation', 'Adult Swim', NULL),
-(14, 'The Boys', 2019, 2, 'Action', 'Amazon Prime', NULL),
-(15, 'Money Heist', 2017, 5, 'Thriller', 'Netflix', NULL);
+(1, 'Breaking Bad', 2008, 5, 'Drama', 'Netflix', '../uploads/default_series/BreakingBad.jpg'),
+(2, 'Friends', 1994, 10, 'Comedy', 'HBO Max', '../uploads/default_series/Friends.jpg'),
+(3, 'Stranger Things', 2016, 4, 'Sci-fi', 'Netflix', '../uploads/default_series/StrangerThings.jpg'),
+(4, 'BoJack Horseman', 2014, 6, 'Animation', 'Netflix', '../uploads/default_series/BoJackJorseman.jpg'),
+(5, 'Game of Thrones', 2011, 8, 'Action', 'HBO', '../uploads/default_series/GameofThrones.jpg'),
+(6, 'The Mandalorian', 2019, 2, 'Sci-fi', 'Disney+', '../uploads/default_series/TheMandalorian.jpg'),
+(7, 'The Haunting of Hill House', 2018, 2, 'Horror', 'Netflix', '../uploads/default_series/TheHauntingofHillHouse.jpg'),
+(8, 'The Handmaid\'s Tale', 2017, 4, 'Drama', 'Hulu', '../uploads/default_series/TheHandmaidsTale.jpg'),
+(9, 'The Witcher', 2019, 2, 'Fantasy', 'Netflix', '../uploads/default_series/TheWitcher.jpg'),
+(10, 'Westworld', 2016, 3, 'Sci-fi', 'HBO', '../uploads/default_series/Westworld.jpg'),
+(11, 'Brooklyn Nine-Nine', 2013, 8, 'Comedy', 'NBC', '../uploads/default_series/BrooklynNineNine.jpg'),
+(12, 'Loki', 2021, 1, 'Action', 'Disney+', '../uploads/default_series/Loki.jpg'),
+(13, 'Rick and Morty', 2013, 5, 'Animation', 'Adult Swim', '../uploads/default_series/RickandMorty.jpg'),
+(14, 'The Boys', 2019, 2, 'Action', 'Amazon Prime', '../uploads/default_series/TheBoys.jpg'),
+(15, 'Money Heist', 2017, 5, 'Thriller', 'Netflix', '../uploads/default_series/MoneyHeist.jpg');
 
 -- --------------------------------------------------------
 
@@ -77,7 +77,10 @@ CREATE TABLE `my_playlists` (
 INSERT INTO `my_playlists` (`playlist_id`, `user_id`, `playlist_name`) VALUES
 (4, 6, 'Watched'),
 (5, 6, 'Currently watching'),
-(6, 6, 'Wishlist');
+(6, 6, 'Wishlist'),
+(7, 7, 'Watched'),
+(8, 7, 'Currently watching'),
+(9, 7, 'Wishlist');
 
 -- --------------------------------------------------------
 
@@ -109,7 +112,7 @@ CREATE TABLE `user_accounts` (
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `avatar` int(11) DEFAULT 1
+  `avatar` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -117,7 +120,8 @@ CREATE TABLE `user_accounts` (
 --
 
 INSERT INTO `user_accounts` (`id`, `name`, `email`, `password`, `avatar`) VALUES
-(6, 'Hilal', 'hallo@hallo.de', '$2y$10$mmLYAUc0izaDd0FcfbEBEutbDpRZg9PTBKrxjd6FQogUeJE9s8K8S', 0);
+(6, 'Hilal', 'hallo@hallo.de', '$2y$10$ASdlVFyzB2cm0n7kNN4am.SKeMIPDxK5xSv0/2aqWW0tO.dOoYgJS', NULL),
+(7, 'Hilal', 'h.cub@web.de', '$2y$10$wFkYt3Gd1iqwWqJ4YA5UiecOLg5Z2vtJTo8LkqnxlW.ne4GX0mCkO', NULL);
 
 --
 -- Indizes der exportierten Tabellen
@@ -154,7 +158,7 @@ ALTER TABLE `user_accounts`
 -- AUTO_INCREMENT für Tabelle `my_playlists`
 --
 ALTER TABLE `my_playlists`
-  MODIFY `playlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `playlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT für Tabelle `my_series`
@@ -166,7 +170,7 @@ ALTER TABLE `my_series`
 -- AUTO_INCREMENT für Tabelle `user_accounts`
 --
 ALTER TABLE `user_accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints der exportierten Tabellen
