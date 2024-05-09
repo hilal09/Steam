a73f0e4fd2b4cd16c59eb9ece4627585016b57c0
yudum-y <yudumyilmaz32@gmail.com>
2024-05-09 02:46:01 +0200

    navigation bar icons

--- www/functions/navigation.php
+++ www/functions/navigation.php
@@ -1,9 +1,20 @@
-<nav class="navbar">
-    <ul>
-    <a href="../functions/logout.php">Logout</a>
-
-        <li><a href="../pages/dashboard.php" <?php if(basename($_SERVER['PHP_SELF']) == 'dashboard.php') echo 'class="active"'; ?>>Home</a></li>
-        <li><a href="../pages/search.php" <?php if(basename($_SERVER['PHP_SELF']) == 'search.php') echo 'class="active"'; ?>>Search</a></li>
-        <li><a href="../pages/profile.php" <?php if(basename($_SERVER['PHP_SELF']) == 'profile.php') echo 'class="active"'; ?>>Profile</a></li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Bar</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
+</head>
+<body>
+    <nav class="navbar">
+        <ul>
+            <li><a href="../functions/logout.php">Logout</a></li>
+            <li><a href="../pages/dashboard.php" <?php if(basename($_SERVER['PHP_SELF']) == 'dashboard.php') echo 'class="active"'; ?>><i class="fa-solid fa-house"></i>Home</a></li>
+            <li><a href="../pages/search.php" <?php if(basename($_SERVER['PHP_SELF']) == 'search.php') echo 'class="active"'; ?>><i class="fa-solid fa-magnifying-glass"></i>Search</a></li>
+            <li><a href="../pages/profile.php" <?php if(basename($_SERVER['PHP_SELF']) == 'profile.php') echo 'class="active"'; ?>><i class="fa-solid fa-user"></i>Profile</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
